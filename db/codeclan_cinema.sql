f7fcc784528e413a2b626e68247d54fc11afdaf8
Stephen Ramsay <rabrandom@gmail.com>
2019-12-06 12:23:16 +0000

    add sql

---
+++ db/codeclan_cinema.sql
@@ -0,0 +1,19 @@
+DROP TABLE customers;
+DROP TABLE films;
+DROP TABLE tickets;
+
+CREATE TABLE tickets(
+  id SERIAL PRIMARY KEY,
+  customer_id INT,
+  film_id INT
+);
+
+CREATE TABLE films(
+  title VARCHAR(255).
+  price INT
+);
+
+CREATE TABLE customers(
+  name VARCHAR(255),
+  funds INT
+);
